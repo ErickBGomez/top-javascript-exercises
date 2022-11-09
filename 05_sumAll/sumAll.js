111e4bf84818211b7a7b275648c83aef279f9662
Erick B. Gómez <erickb.gomez28@gmail.com>
2022-11-09 10:00:24 -0600

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    let initialValue;
+    let finalValue;
 
+    // Check if parameters are ONLY positive integer numbers (including the 0)
+    for (let num of arguments) {
+        if (num < 0 || typeof num !== "number") return "ERROR";
+    }
+
+    // Ensure that initialValue is less than finalValue
+    if (firstNum > secondNum) {
+        initialValue = secondNum;
+        finalValue = firstNum;
+    } else {
+        initialValue = firstNum;
+        finalValue = secondNum;
+    }
+
+    // Add all integers between initialValue and finalValue (inclusive)
+    for (let i = initialValue; i <= finalValue; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
