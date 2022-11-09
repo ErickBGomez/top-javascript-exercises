8dbe2673ca79c5426a457b4e25d0b8cc214ae9b2
Erick B. Gómez <erickb.gomez28@gmail.com>
2022-11-08 21:44:45 -0600

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...prompts) {
+    for (let prompt of prompts) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === prompt) array.splice(i, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
