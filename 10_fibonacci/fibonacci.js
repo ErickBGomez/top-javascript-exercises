db8d37d22be9571e5b2d0cc5895e6f97dd31a301
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-16 16:02:06 -0600

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const sequence = [0, 1, 1];
 
+    if (n < 0) return "OOPS";
+    if (n <= 2) return sequence[n];
+
+    for (let i = 0; i < n - 2; i++) {
+        sequence[0] = sequence[1];
+        sequence[1] = sequence[2];
+
+        sequence[2] = sequence[0] + sequence[1];
+    }
+
+    return sequence[2];
 };
 
 // Do not edit below this line
