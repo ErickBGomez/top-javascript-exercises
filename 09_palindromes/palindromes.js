32000c7a6d6eeeaa33a8d01bc6f733935a539c87
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-16 11:42:18 -0600

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const lowerCaseText = text.toLowerCase();
+    const stringArray = Array.from(lowerCaseText).filter((char) => char >= "a" && char <= "z");
 
+    return stringArray.every((char, index, array) => char === array[array.length - 1 - index]);
 };
 
 // Do not edit below this line
