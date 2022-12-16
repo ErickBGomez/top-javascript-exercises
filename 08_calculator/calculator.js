fedc5c09d15290edc23b10fe8c5a21261184a6c3
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-16 11:15:42 -0600

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce((accumulator, num) => accumulator + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return (numArray.length
+        ? numArray.reduce((accumulator, num) => accumulator * num)
+        : 0); // If array is empty, return 0
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  
+  let result = num;
+
+  for (let i = num-1; i >= 1; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
