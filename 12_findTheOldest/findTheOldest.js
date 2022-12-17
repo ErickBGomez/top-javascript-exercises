ecb140eafb6d0c5adcd1dc6c5addf8b283dec7f8
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-17 09:49:57 -0600

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const currentYear = new Date().getFullYear();
+
+    let oldestToYoungest = array.sort((a, b) => {
+        const currentPerson = (a.yearOfDeath ?? currentYear) - a.yearOfBirth;
+        const nextPerson = (b.yearOfDeath ?? currentYear) - b.yearOfBirth;
+
+        return (currentPerson > nextPerson) ? -1 : 1;
+    });
+
+    return oldestToYoungest[0];
 };
 
 // Do not edit below this line
